22bb5f46fad2d76c54d44cf6a1a0130e60462e7a
AliyanAli911 <aliyanali723033@gmail.com>
2026-06-09 21:18:27 +0500

    Fix Express 5 wildcard route syntax for static file serving

--- server/index.js
+++ server/index.js
@@ -165,7 +165,7 @@ app.post('/api/lead', async (req, res) => {
 // Serve built React frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')))
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'))
   })
 }
